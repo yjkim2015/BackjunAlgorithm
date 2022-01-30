cac6dfb210aef84498568b6c0ce6d7b8032c2d03
kimyoonje <forteas2003@hanmail.net>
2022-01-30 21:58:28 +0900

    [김윤제] 수학 - 팩토리얼의 0의 개수

---
+++ src/math/Factorial.java
@@ -0,0 +1,18 @@
+package math;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int count = 0;
+        for(int i = 5; i <= n; i*=5) {
+            count += n/i;
+        }
+        System.out.println(count);
+    }
+}
