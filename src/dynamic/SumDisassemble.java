44a79b3b2a37826f0e07b8a120da437aae89de48
yoonoa <yoonoa@tmon.co.kr>
2022-02-20 20:50:35 +0900

    [김윤제] 합분해

---
+++ src/dynamic/SumDisassemble.java
@@ -0,0 +1,28 @@
+package dynamic;
+
+import java.util.Scanner;
+
+public class SumDisassemble {
+    private static final int MOD = 1000000000;
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[][] d = new int[k+1][n+1];
+
+        d[0][0] = 1;
+
+        for (int i = 1; i <= k; i++) {
+            for (int j = 0; j <= n; j++) {
+                for (int l = 0; l <= j; l++) {
+                    d[i][j] += (d[i-1][j-l])%MOD;
+                }
+            }
+        }
+
+        System.out.println(d[k][n]%MOD);
+    }
+}
